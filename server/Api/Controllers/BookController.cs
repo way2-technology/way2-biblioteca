60204a9810c9efb3bd03e8800cc4fc35f563ee30
Thiago Cerutti <thiagocerutti@gmail.com>
2018-08-18 21:14:08 -0300

    Facilitador para manutenção das informações dos livros

--- server/Api/Controllers/BookController.cs
+++ server/Api/Controllers/BookController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Api.Controllers
-{
-    public class BookController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace Api.Controllers {
+    public class BookController : Controller {
+        private IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository) {
+            _bookRepository = bookRepository;
+        }
+
+        public IActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(Book book) {
+            _bookRepository.Save(book);
+            return Redirect(Url.Action("Index", "Home"));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            var book = _bookRepository.Load(id);
+            return View("Edit", book);
+        }
+
+        [HttpGet]
+        // TODO: método para popular arquivo de dados do MiniBiggy com os dados do JOSN original. Pode ser apagado assim que a base de dados estiver toda verificada.
+        public IActionResult PopulateMiniBiggy() {
+            var books = _bookRepository.ListAll(0, int.MaxValue);
+            _bookRepository.Save(books);
+            return Redirect(Url.Action("Index", "Home"));
         }
     }
 }
